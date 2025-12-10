31f4882cb37a6429ae2f34a240a45126bcf0dc50
sheldonHarrison <sharrison0092@kctcs.edu>
2025-12-10 03:00:29 -0500

    Add files via upload

---
+++ Xbox or Playstation/consoles/images/scripts/script.js
@@ -0,0 +1,15 @@
+/*
+    Author: Sheldon harrison
+    File Name: scripts.js
+    Date: 12/10/2025
+*/
+
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
